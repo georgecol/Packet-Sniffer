80be933b3e9f8641631d5dd54946d656d675b398
goeg1 <gcollier229@gmail.com>
2025-06-26 23:35:00 +1200

    Setting up to capture more packets
    Store packets in hashmap, display details of each packet, even if they are not ipv4 packets (ipv6, arp, etc).

---
+++ NetworkTrafficAnalysis/src/main/java/Model/Packet.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.net.InetAddress;
+import org.pcap4j.packet.namednumber.IpNumber;
+
+/**
+ *
+ * @author George
+ */
+public class Packet {
+
+    private IpNumber protocol;
+    private InetAddress srcIp;
+    private InetAddress dstIp;
+    private int totalLength;
+
+    public Packet(IpNumber protocol, InetAddress srcIp, InetAddress dstIp, int totalLength) {
+        this.protocol = protocol;
+        this.srcIp = srcIp;
+        this.dstIp = dstIp;
+        this.totalLength = totalLength;
+    }
+
+    public IpNumber getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(IpNumber protocol) {
+        this.protocol = protocol;
+    }
+
+    public InetAddress getSrcIp() {
+        return srcIp;
+    }
+
+    public void setSrcIp(InetAddress srcIp) {
+        this.srcIp = srcIp;
+    }
+
+    public InetAddress getDstIp() {
+        return dstIp;
+    }
+
+    public void setDstIp(InetAddress dstIp) {
+        this.dstIp = dstIp;
+    }
+
+    public int getTotalLength() {
+        return totalLength;
+    }
+
+    public void setTotalLength(int totalLength) {
+        this.totalLength = totalLength;
+    }
+}
--- NetworkTrafficAnalysis/src/main/java/Model/PacketCapture.java
+++ NetworkTrafficAnalysis/src/main/java/Model/PacketCapture.java
@@ -8,6 +8,7 @@ import java.io.EOFException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.HashMap;
 import java.util.concurrent.TimeoutException;
 import org.pcap4j.core.NotOpenException;
 import org.pcap4j.core.PcapNetworkInterface;
@@ -27,10 +28,14 @@ public class PacketCapture {
     public static InetAddress addr;
     public static PcapHandle handle;
     public static Packet packet;
-    public static int timeout;
+    public static int timeout = 1000;
     public static boolean packetCaptured;
+    public static HashMap<Integer, Packet> packets;
 
-    public static void main(String[] args) throws PcapNativeException, NotOpenException, EOFException {
+    public static void main(String[] args) {
+
+        packets = new HashMap<>();
+
         String nicAddress = "192.168.0.155"; // Current ethernet NIC address, in future can replace and get dynamically depending on chosen NIC
         try {
             addr = InetAddress.getByName(nicAddress); // get address to pass to pcap
@@ -39,41 +44,52 @@ public class PacketCapture {
             System.out.println(e.getMessage());
         }
 
-        PcapNetworkInterface nif = Pcaps.getDevByAddress(addr); // Find the network interface that you want to capture packets
-
         //Open Pcap Handle
         try {
+            PcapNetworkInterface nif = Pcaps.getDevByAddress(addr); // Find the network interface that you want to capture packets
             int snapLen = 65536;
             PromiscuousMode mode = PromiscuousMode.PROMISCUOUS;
-            timeout = 30;
             handle = nif.openLive(snapLen, mode, timeout);
         } catch (PcapNativeException e) {
             System.out.println(e.getMessage());
         }
         //Capture packet
-        packetCaptured = true;
-        try {
-            packet = handle.getNextPacketEx(); //throws PcapNativeException, EOFException, NotOpenException
-            handle.close();
-        } catch (TimeoutException e) {
-            packetCaptured = false;
-            System.out.println("No packet captured in: " + timeout+"ms");
-            System.out.println(e.getMessage());
-        }
+        int packetLimit = 10;
+        for (int i = 1; i <= packetLimit; i++) {
+            System.out.println("Packet #" + i);
+            packetCaptured = true;
+            try {
+                packet = handle.getNextPacketEx();
 
-        //Print Packet Information
-        if (packetCaptured) {
-            IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
-            Inet4Address srcAddr = ipV4Packet.getHeader().getSrcAddr();
-            displayCapturedPacket(ipV4Packet);
-        }
+            } catch (EOFException | TimeoutException | NotOpenException | PcapNativeException e) {
+                packetCaptured = false;
+                System.out.println("No packet captured in: " + timeout + "ms");
+                System.out.println(e.getMessage());
+            }
 
+            //Print Packet Information for each packet
+            if (packetCaptured) {
+                IpV4Packet ipV4Packet = packet.get(IpV4Packet.class);
+
+                if (ipV4Packet != null) { // Checks if packet is Ip , if packetcaptured and not ip , dont attempt to display , because build is only for ipv4, will give error
+                    packets.put(i, packet); // store in packets map
+                    displayCapturedPacket(ipV4Packet);
+                } else {
+                    System.out.println("Captured non-IPv4 packet: " + packet);
+                }
+            }
+
+        }
+        handle.close();
     }
 
     public static void displayCapturedPacket(IpV4Packet packet) {
-        System.out.println("Packet Captured!");
-        System.out.print("  SRC: " + packet.getHeader().getSrcAddr());
-        System.out.print("  DST: " + packet.getHeader().getDstAddr());
-        System.out.print("  Protocol: " + packet.getHeader().getProtocol());
+//        System.out.println("Packet Captured!");
+//        System.out.print("  SRC: " + packet.getHeader().getSrcAddr());
+//        System.out.print("  DST: " + packet.getHeader().getDstAddr());
+//        System.out.print("  Protocol: " + packet.getHeader().getProtocol());
+
+        System.out.println("Build: " + packet.getBuilder().build());
+
     }
 }
